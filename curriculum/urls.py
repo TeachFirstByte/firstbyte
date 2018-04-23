81f8cd3fd991ddf8fd0ac17dbda2b257dbf0c28b
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2018-04-23 14:00:16 -0400

    Add trailing slashes to all urls in curriculum.urls

--- curriculum/urls.py
+++ curriculum/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('lesson-plan/new', views.CreateLessonPlanView.as_view(), name='create-lesson-plan'),
-    path('lesson-plan/<int:pk>', views.LessonPlanView.as_view(), name='detail-lesson-plan'),
-    path('lesson-plan/all', views.LessonPlanList.as_view(), name='list-lesson-plan'),
+    path('lesson-plan/new/', views.CreateLessonPlanView.as_view(), name='create-lesson-plan'),
+    path('lesson-plan/<int:pk>/', views.LessonPlanView.as_view(), name='detail-lesson-plan'),
+    path('lesson-plan/all/', views.LessonPlanList.as_view(), name='list-lesson-plan'),
 ]

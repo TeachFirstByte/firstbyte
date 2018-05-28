ebc85839e33290c54685b003fe85110579f3b683
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2018-05-28 18:59:11 -0400

    Use urls in curriculum app without a prefix

--- firstbyte/urls.py
+++ firstbyte/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('curriculum/', include('curriculum.urls')),
     path('user/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('', include('curriculum.urls')),
 ]

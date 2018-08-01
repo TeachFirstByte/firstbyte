570c158935a7af35ed58b9a9c7d4006b2937f1e4
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2018-07-31 22:31:01 -0400

    Add FAQ page
    There's a link to it in the footer, which was slightly rearranged.

--- firstbyte/urls.py
+++ firstbyte/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from landing.views import index
+from landing.views import index, faq
 
 urlpatterns = [
 
     path('user/', include('accounts.urls')),
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('frequently-asked-questions/', faq, name='faq'),
     path('', include('curriculum.urls')),
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'landing/landing.html', {'user': request.user})
+
+def faq(request):
+    return render(request, 'landing/faq.html', {'user': request.user})

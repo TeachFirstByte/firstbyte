91d0de4db76e818b913d25bd0dda955d48d2c318
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2018-08-08 17:43:09 -0400

    Fix redirect for missing slug

--- curriculum/views.py
+++ curriculum/views.py
@@ -54,7 +54,7 @@ def slug_redirect_view(klass, to=None, permanent=True, *args, **kwargs):
     def view_fn(request, pk):
         obj = get_object_or_404(klass, id=pk)
         target = to or klass
-        return redirect(target, *args, pk=pk, slug=obj.slug, permanent=permanent, **kwargs)
+        return redirect(obj, *args, pk=pk, slug=obj.slug, permanent=permanent, **kwargs)
     return view_fn
 
 class LessonPlanView(View):

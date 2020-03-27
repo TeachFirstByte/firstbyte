eaa6588ad78dfcdfdcc5f3eea74d2d94ac45179c
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-03-27 19:58:41 -0400

    Add BootstrapVue

--- curriculum/static/js/curriculumForm.js
+++ curriculum/static/js/curriculumForm.js
@@ -3,9 +3,17 @@
 import $ from 'jquery';
 
 import Vue from 'vue';
+
+import { BootstrapVue } from 'bootstrap-vue'
+
 import TheCurriculumForm from './components/TheCurriculumForm.vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 $(() => {
+    Vue.use(BootstrapVue)
+
     var vm = new Vue({
         el: "#vue-container",
         components: {

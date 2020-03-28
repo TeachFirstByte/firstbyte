8f2c40f9fdd4971e75aa355a2ada4be410568b14
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-03-28 17:28:16 -0400

    Initial work on new curriculum submission form

--- curriculum/static/js/components/TheCurriculumForm.vue
+++ curriculum/static/js/components/TheCurriculumForm.vue
@@ -1,15 +1,262 @@
 <template>
-    <h1>Hello World</h1>
+    <b-container class="my-5">
+        <b-row>
+            <b-col>
+                <h4>Curriculum Information</h4>
+            </b-col>
+            <b-col>
+                <h4>Upload Supporting Files</h4>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-row>
+                    <b-col>
+                        <b-form-group label="Title:" label-for="title-input">
+                            <b-form-input
+                                id="title-input"
+                                type="text"
+                                v-model="$v.title.$model"
+                                :state="getBootstrapFormInputState($v.title)"
+                            ></b-form-input>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <b-form-group label="Summary:" label-for="summary-input">
+                            <b-form-textarea
+                                id="summary-input"
+                                v-model="$v.summary.$model"
+                                :state="getBootstrapFormInputState($v.summary)"
+                            ></b-form-textarea>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <b-form-group label="Grade Level:" label-for="grade-level-input">
+                            <b-form-select
+                                id="grade-level-input"
+                                :options="gradeLevelOptions"
+                                v-model="$v.gradeLevel.$model"
+                                :state="getBootstrapFormInputState($v.gradeLevel)"
+                            ></b-form-select>
+                        </b-form-group>
+                    </b-col>
+                    <b-col>
+                        <b-form-group label="Num Classes:" label-for="num-classes-input">
+                            <b-form-input
+                                id="num-classes-input"
+                                type="number"
+                                min="1"
+                                v-model="$v.numClasses.$model"
+                                :state="getBootstrapFormInputState($v.numClasses)"
+                            ></b-form-input>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <b-form-group label="Single Class Time:" label-for="single-class-time-input">
+                            <b-form-select
+                                id="single-class-time-input"
+                                :options="durationOptions"
+                                v-model="$v.singleClassTime.$model"
+                                :state="getBootstrapFormInputState($v.singleClassTime)"
+                            ></b-form-select>
+                        </b-form-group>
+                    </b-col>
+                    <b-col>
+                        <b-form-group label="Total Prep Time:" label-for="total-prep-time-input">
+                            <b-form-select
+                                id="total-prep-time-input"
+                                :options="durationOptions"
+                                v-model="$v.totalPrepTime.$model"
+                                :state="getBootstrapFormInputState($v.totalPrepTime)"
+                            ></b-form-select>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <b-form-group label="Materials:" label-for="materials-input">
+                            <b-form-textarea
+                                id="materials-input"
+                                v-model="$v.materials.$model"
+                                :state="getBootstrapFormInputState($v.materials)"
+                            ></b-form-textarea>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <b-form-group>
+                            <b-form-checkbox
+                                id="web-only-input"
+                                v-model="webOnly"
+                            >
+                                Web only
+                            </b-form-checkbox>
+                        </b-form-group>
+                    </b-col>
+                    <b-col>
+                        <b-form-group>
+                            <b-form-checkbox
+                                id="feedback-enabled-input"
+                                v-model="feedbackEnabled"
+                            >
+                                Allow reviews
+                            </b-form-checkbox>
+                        </b-form-group>
+                    </b-col>
+                    <b-col>
+                        <b-form-group>
+                            <b-form-checkbox
+                                id="draft-input"
+                                v-model="draft"
+                            >
+                                This a draft
+                            </b-form-checkbox>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+                <b-row>
+                    <b-col>
+                        <b-form-group>
+                            <b-form-checkbox
+                                id="agree-input"
+                                v-model="$v.agree.$model"
+                                :state="getBootstrapFormInputState($v.agree)"
+                            >
+                                I am the sole author or I have permission to license this work under the CC BY-NC 4.0 License
+                            </b-form-checkbox>
+                        </b-form-group>
+                    </b-col>
+                </b-row>
+            </b-col>
+            <b-col fluid="sm" class="d-flex flex-column">
+                <span class="lr-additional-clickarea mb-3">Attach materials by dragging &amp; dropping or clicking in the box below.</span>
+                <div class="lr-droparea">
+                    <input type="file" class="lr-file-input" id="file-select" multiple />
+                    <div id="lesson-resources" class="flex-fill">
+                        <div class="lr-fallback">
+                            File uploads are not supported in this browser. You may wish to try the following:
+                            <ul class="text-left">
+                                <li>Updating your browser</li>
+                                <li>Enabling Javascript</li>
+                                <li>Contact <a href="mailto:info@teachfirstbyte.org">info@teachfirstbyte.org</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </b-col>
+        </b-row>
+        <p class="lesson-submission-warning">
+            Please see our <a href="{% url 'faq' %}">FAQ</a> for more information.
+        </p>
+        <b-row class="w-100 text-center">
+            <b-col class="text-center">
+                <input type="submit" class="btn btn-primary mt-3" for="lesson-plan-form" id="submit-lesson-plan" value="Upload Curriculum"/>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 <script>
-export default {
-    data: function() {
-        return {
+    import { validationMixin } from 'vuelidate'
+    import { required, minValue } from 'vuelidate/lib/validators'
 
+    export default {
+        mixins: [validationMixin],
+        data() {
+            return {
+                title: "",
+                summary: "",
+                gradeLevel: null,
+                gradeLevelOptions: [
+                    { value: null, text: "" },
+                    { value: "ES", text: "Elementary School" },
+                    { value: "MS", text: "Middle School" },
+                    { value: "HS", text: "High School" },
+                    { value: "U", text: "Post-secondary" },
+                ],
+                numClasses: null,
+                totalPrepTime: null,
+                singleClassTime: null,
+                durationOptions: [
+                    { value: null, text: ''},
+                    { value: '0:00', text: '0:00' },
+                    { value: '0:15', text: '0:15' },
+                    { value: '0:30', text: '0:30' },
+                    { value: '0:45', text: '0:45' },
+                    { value: '1:00', text: '1:00' },
+                    { value: '1:15', text: '1:15' },
+                    { value: '1:30', text: '1:30' },
+                    { value: '1:45', text: '1:45' },
+                    { value: '2:00', text: '2:00' },
+                    { value: '2:15', text: '2:15' },
+                    { value: '2:30', text: '2:30' },
+                    { value: '2:45', text: '2:45' },
+                    { value: '3:00', text: '3:00' },
+                    { value: '3:15', text: '3:15' },
+                    { value: '3:30', text: '3:30' },
+                    { value: '3:45', text: '3:45' },
+                    { value: '4:00', text: '4:00' },
+                    { value: '4:15', text: '4:15' },
+                    { value: '4:30', text: '4:30' },
+                    { value: '4:45', text: '4:45' },
+                    { value: '5:00', text: '5:00' },
+                    { value: '5:15', text: '5:15' },
+                    { value: '5:30', text: '5:30' },
+                    { value: '5:45', text: '5:45' },
+                    { value: '6:00', text: '6:00' },
+                ],
+                materials: "",
+                webOnly: false,
+                feedbackEnabled: false,
+                draft: false,
+                agree: false,
+            }
+        },
+        validations: {
+            title: {
+                required,
+            },
+            summary: {
+                required,
+            },
+            gradeLevel: {
+                required,
+            },
+            numClasses: {
+                required,
+                minValue: minValue(1)
+            },
+            totalPrepTime: {
+                required,
+            },
+            singleClassTime: {
+                required,
+            },
+            materials: {
+                required,
+            },
+            agree: {
+                mustAgree: (value) => value
+            },
+        },
+        methods: {
+            getBootstrapFormInputState(vuelidateObject) {
+                if (vuelidateObject.$dirty) {
+                    return !vuelidateObject.$error;
+                }
+                return null;
+            }
         }
     }
-}
 </script>
 <style lang="scss">
-
+    b-form-select {
+        appearance: none;
+    }
 </style>

56e2e2bc228906ca44e32220634195090332a8f9
Khalil Haji <khalilalihaji@gmail.com>
2020-04-01 12:45:00 -0700

    Begins implmenting top level Vue component for loaner program.

--- loaner_program/static/loaner_program/js/components/TheLoanerProgram.vue
+++ loaner_program/static/loaner_program/js/components/TheLoanerProgram.vue
@@ -1,4 +1,20 @@
 <template>
+  <div class="container">
+    <div class="loaner-content">
+      <div class="loaner-intro">
+	<h1 class="font-weight-bold">Loaner Program</h1>
+	<p>Borrow electronics from FirstByte for your classroom</p>
+	
+	<p>If you're in the Boston area and need materials, we're here to
+	  help. FirstByte's loaner program gives local educators the chance
+	  to borrow various technology kits to use in their clasrooms.</p>
+      </div>
+      <hr>
+      <p class="font-weight-bold">Our Inventory</p>
+      <p>Please note: you must be logged in to reserve our technology kits.</p>
+      <kit />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,5 +23,19 @@ export default {
 </script>
 
 <style scoped>
+.loaner-content {
+    width: 100%;
+    height: 20em;
+}
+.loaner-intro {
+    display: flex;
+    width: 40%;
+    flex-direction: column;
+}
+
+hr {
+    margin: 1.5rem 0 auto;
+}
+
 
 </style>

1a41d7e0dbf1b56925888b23c915884c76121821
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-04-07 23:36:59 -0400

    Add font awesome as dependency

--- curriculum/static/js/curriculumForm.js
+++ curriculum/static/js/curriculumForm.js
@@ -9,9 +9,12 @@ import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 import TheCurriculumForm from './components/TheCurriculumForm.vue';
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 $(() => {
     Vue.use(BootstrapVue);
+
+    Vue.component('font-awesome-icon', FontAwesomeIcon);
 
     new Vue({
         el: "#vue-container",

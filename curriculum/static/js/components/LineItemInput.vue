41dcd70de314d80b8ced826d94ced3204012c0ba
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-04-11 18:40:03 -0400

    Make material line items draggable

--- curriculum/static/js/components/LineItemInput.vue
+++ curriculum/static/js/components/LineItemInput.vue
@@ -1,37 +1,54 @@
 <template>
     <div>
-        <div
-            v-for="(lineItem, index) in value"
-            :key="index"
-            class="d-flex mb-2"
+        <draggable
+            v-modal="valueProp"
+            handle=".grip-handle"
         >
-            <input
-                ref="itemInputs"
-                class="flex-grow-1 mr-1 form-control"
-                :class="{'is-valid': isValidAtIndex(index), 'is-invalid': isInvalidAtIndex(index)}"
-                :value="lineItem"
-                @input="onInput(index, $event.target.value)"
-                @keyup.enter="onInsertAfterIndexAndFocus(index)"
+            <div
+                v-for="(lineItem, index) in value"
+                :key="index"
+                class="d-flex mb-2"
             >
-            <font-awesome-icon
-                v-if="index == value.length - 1"
-                class="text-secondary align-self-center"
-                :icon="['fas', 'plus-circle']"
-                size="lg"
-                @click="onAppendNewItem"
-            />
-            <font-awesome-icon
-                v-else
-                class="text-danger align-self-center"
-                :icon="['fas', 'minus-circle']"
-                size="lg"
-                @click="onRemoveItemAtIndex(index)"
-            />
-        </div>
+                <div class="grip-handle text-dark pr-2 align-self-center">
+                    <font-awesome-icon
+                        class="my-auto"
+                        :icon="['fas', 'grip-lines-vertical']"
+                        size="lg"
+                    />
+                </div>
+                <input
+                    ref="itemInputs"
+                    class="flex-grow-1 mr-2 form-control"
+                    :class="{'is-valid': isValidAtIndex(index), 'is-invalid': isInvalidAtIndex(index)}"
+                    :value="lineItem"
+                    @input="onInput(index, $event.target.value)"
+                    @keyup.enter="onInsertAfterIndexAndFocus(index)"
+                >
+                <font-awesome-icon
+                    v-if="index == value.length - 1"
+                    class="text-secondary align-self-center"
+                    :icon="['fas', 'plus-circle']"
+                    size="lg"
+                    @click="onAppendNewItem"
+                />
+                <font-awesome-icon
+                    v-else
+                    class="text-danger align-self-center"
+                    :icon="['fas', 'minus-circle']"
+                    size="lg"
+                    @click="onRemoveItemAtIndex(index)"
+                />
+            </div>
+        </draggable>
     </div>
 </template>
 <script>
+    import draggable from 'vuedraggable';
+
     export default {
+        components: {
+            draggable,
+        },
         props: {
             value: {
                 type: Array,
@@ -40,6 +57,16 @@
             states: {
                 type: Array,
                 default: () => { return []; },
+            },
+        },
+        computed: {
+            valueProp: {
+                get: function() {
+                    return this.value;
+                },
+                set: function(newValue) {
+                    this.$emit('update', newValue);
+                },
             },
         },
         methods: {

1760922e31944dc2b72fb5969687b9cfd6f64564
Luke San Antonio Bialecki <lukesanantonio@gmail.com>
2020-05-09 14:25:59 -0400

    Use session authentication

--- firstbyte/settings.py
+++ firstbyte/settings.py
@@ -80,8 +80,8 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_RENDERER_CLASSES': (
         'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
